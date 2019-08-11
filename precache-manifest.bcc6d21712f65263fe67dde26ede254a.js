4b96fdd63451580117b6aa55b0b6533fffc1023c
Guillaume <guillaume.odendaal@gmail.com>
2019-08-11 19:11:23 +0200

    Updates

---
+++ precache-manifest.bcc6d21712f65263fe67dde26ede254a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1474e6d21b87e2f6ee2bceceebc8c1d7",
+    "url": "/imberbe/index.html"
+  },
+  {
+    "revision": "eb6633d42e9b98b35540",
+    "url": "/imberbe/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f708f1b6ddc611855c0a",
+    "url": "/imberbe/static/css/main.1f998af9.chunk.css"
+  },
+  {
+    "revision": "eb6633d42e9b98b35540",
+    "url": "/imberbe/static/js/2.90e19b21.chunk.js"
+  },
+  {
+    "revision": "f708f1b6ddc611855c0a",
+    "url": "/imberbe/static/js/main.e00aed68.chunk.js"
+  },
+  {
+    "revision": "96ae0dabc4718420002c",
+    "url": "/imberbe/static/js/runtime~main.0a7e0729.js"
+  },
+  {
+    "revision": "3cc0860722158a5e95d7980067bacf11",
+    "url": "/imberbe/static/media/SmashWall-PersonalUse.3cc08607.woff"
+  },
+  {
+    "revision": "e84dbb1d7240553c10e61ca471c48828",
+    "url": "/imberbe/static/media/enviroment.e84dbb1d.png"
+  },
+  {
+    "revision": "06ae42f4d9cab4f0fdb260a6632b16e8",
+    "url": "/imberbe/static/media/location.06ae42f4.png"
+  },
+  {
+    "revision": "81780086d9d3477c9e3bd02509b76887",
+    "url": "/imberbe/static/media/top.81780086.jpg"
+  },
+  {
+    "revision": "8243f86e48f676757396408179c055a0",
+    "url": "/imberbe/static/media/wildlife.8243f86e.png"
+  }
+]);
