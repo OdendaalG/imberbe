c31fdd2f0e143af8e95bddc84ac0146f78d4554d
OdendaalG <guillaume.odendaal@gmail.com>
2020-05-24 09:07:06 +0200

    Hover added with blue background

--- src/Components/BottomInfo.js
+++ src/Components/BottomInfo.js
@@ -7,13 +7,13 @@ import loc from '../img/location.png';
 function BottomInfo(){
   return(
     <div className="dt-ns dt--fixed-ns" id="Bottom">
-      <div className="dtc-ns tc pv4 ph2">
+      <div className="hover-enlarge dtc-ns tc pv4 ph2">
         <a href='./index.js'><img src={env} alt='Bush veld and road' /></a>
       </div>
-      <div className="dtc-ns tc pv4 ph2">
+      <div className="hover-enlarge dtc-ns tc pv4 ph2">
         <a href='./index.js'><img src={wld} alt='Bush veld and road' /></a>
       </div>
-      <div className="dtc-ns tc pv4 ph2">
+      <div className="hover-enlarge dtc-ns tc pv4 ph2">
         <a href='./index.js'><img src={loc} alt='Bush veld and road' /></a>
       </div>
     </div>
